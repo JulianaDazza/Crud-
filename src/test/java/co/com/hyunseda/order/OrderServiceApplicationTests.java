ae00a7bcc8ef3332b6ec53c230254db203ffe147
Juliana Dazza <julianad@unicauca.edu.co>
2024-05-20 17:00:23 -0500

    Prueba

---
+++ src/test/java/co/com/hyunseda/order/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.hyunseda.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
